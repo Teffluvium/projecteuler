d007557a3cf1d3d78dde014c6bb0ef3da2e4aaf0
Tim Herrin <teffluvium@gmail.com>
2025-05-07 00:56:49 -0700

    Makes target search only look at directories of the form src/problem_*.

--- src/problem_010/problem_010.cpp
+++ src/problem_010/problem_010.cpp
@@ -1,8 +1,8 @@
 /*
  * Problem 10 from ProjectEuler.net
- * 
+ *
  * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
- * 
+ *
  * Find the sum of all the primes below one million.
  */
 
@@ -14,19 +14,18 @@ using namespace std;
 const unsigned int MaxPrime = 1000000;
 int main()
 {
-    unsigned int currPrime = 2, 
-                 num       = 3, 
-                 count     = 1;
+    unsigned int currPrime = 2,
+                 num = 3;
     unsigned long sum = currPrime;
-    bool         isPrime   = 1;
-    double       root      = 0;
-    
+    bool isPrime = 1;
+    double root = 0;
+
     do
     {
         isPrime = 1;
-        root    = sqrt(num);
-        
-        for (unsigned int k=2; k<=root; k++)
+        root = sqrt(num);
+
+        for (unsigned int k = 2; k <= root; k++)
         {
             if (num % k == 0)
             {
@@ -37,12 +36,11 @@ int main()
         if (isPrime)
         {
             currPrime = num;
-            count++;
             sum += currPrime;
         }
         num += 2;
     } while (num < MaxPrime);
-    
+
     cout << "The sum of primes less than " << MaxPrime << " is " << sum << endl;
 
     return 0;
